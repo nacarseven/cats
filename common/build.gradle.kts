04bed5e21f1869f8b586ca1958e16d3d75b920d1
nacarseven <nacarseven@gmail.com>
2023-07-19 10:42:34 -0300

    add common module

---
+++ common/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+
+    defaultConfig {
+        minSdk = AndroidConfigVersions.minSdkVersion
+        targetSdk = AndroidConfigVersions.targetSdkVersion
+        compileSdk = AndroidConfigVersions.compileSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+dependencies {
+
+// Test
+    implementation(TestDependencies.lifecycleViewModel)
+    implementation(TestDependencies.lifecycleRuntime)
+    testImplementation(TestDependencies.junit)
+    testImplementation(TestDependencies.mockk)
+    testImplementation(TestDependencies.coroutinesTest)
+    testImplementation(TestDependencies.archCoreTesting)
+    testImplementation(TestDependencies.turbine)
+    androidTestImplementation(TestDependencies.androidxJunit)
+    androidTestImplementation(TestDependencies.espressoCore)
+}
